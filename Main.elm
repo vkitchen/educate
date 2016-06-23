846055e1d1d1832070d804c6fc3619090c79c247
Vaughan Kitchen <v.kitchen@gnoms.org>
2016-06-24 10:33:59 +1200

    added url routing

--- Main.elm
+++ Main.elm
@@ -8,13 +8,21 @@ import Markdown
 import Http
 import Json.Decode as Json exposing((:=))
 import Task
-import Navigation
+import Nav
+import RouteHash exposing (HashUpdate)
+import RouteUrl exposing (UrlChange)
+import RouteUrl.Builder as Builder exposing (Builder, builder, path, replacePath)
+import String exposing (toInt)
+import RouteUrl
+import Navigation exposing (Location)
 
 (=>) = (,)
 
 main =
-  Html.program
-    { init = init 0
+  RouteUrl.program
+    { delta2url = delta2hash
+    , location2messages = hash2messages
+    , init = init 0
     , view = view
     , update = update
     , subscriptions = subscriptions
@@ -55,6 +63,7 @@ type Msg
   | FetchSucceed (Int, String, String)
   | FetchFail Http.Error
   | Prompt String
+  | Set Int
 
 update action model =
   case action of
@@ -91,6 +100,9 @@ update action model =
     Prompt _ ->
       (model, Cmd.none)
 
+    Set value ->
+      ({model | pageId = value}, getPage value)
+
 -- SUBSCRIPTIONS
 subscriptions model =
   Sub.batch [
@@ -120,7 +132,7 @@ view model =
     [ style
       [ "height" => "100%", "display" => "flex"
       , "flex-direction" => "column" ]]
-    [ Navigation.nav
+    [ Nav.nav
     , windowbox model
     ]
 
@@ -183,6 +195,48 @@ onTab action =
         else Err "not handling that key"))
   in
     onWithOptions "keydown" options decoder
+
+-- ROUTING
+-- Top Level
+delta2hash : Model -> Model -> Maybe UrlChange
+delta2hash previous current =
+    -- Here, we're re-using the Builder-oriented code, but stuffing everything
+    -- into the hash (rather than actually using the full URL).
+    Maybe.map Builder.toHashChange <|
+        delta2builder previous current
+
+delta2builder : Model -> Model -> Maybe Builder
+delta2builder previous current =
+    builder
+    |> replacePath [toString current.pageId]
+    |> Just
+
+--hash2messages : Location -> List Action
+-- Top Level
+hash2messages location =
+    -- You can parse the `Location` in whatever way you want. I'm making
+    -- a `Builder` and working from that, but I'm sure that's not the
+    -- best way. There are links to a number of proper parsing packages
+    -- in the README.
+    builder2messages (Builder.fromHash location.href)
+
+--builder2messages : Builder -> List Action
+builder2messages builder =
+    case path builder of
+        first :: rest ->
+            case toInt first of
+                Ok value ->
+                    [ Set value ]
+
+                Err _ ->
+                    -- If it wasn't an integer, then no action ... we could
+                    -- show an error instead, of course.
+                    []
+
+        _ ->
+            -- If nothing provided for this part of the URL, return empty list
+            []
+
 
 -- STYLES
 windowMenuStyle =
